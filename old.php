f2c1d77ee928a32bc669f68ed014ae1d8298fed8
adarshu <adarshu@yahoo.com>
2016-09-21 17:11:18 -0700

    test

--- old.php
+++ old.php
@@ -1,6 +1,6 @@
 <html lang="en">
 <head>
-    <title>SkipScene </title>
+    <title>SkipScene 2 </title>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
     <meta name="author" content="">
